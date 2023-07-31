78ea6b332ab8d50271803a00648aa99fd553029f
J-Pinos3 <josloco665@gmail.com>
2023-07-31 16:45:46 -0500

    React mmm

---
+++ vaina_vite/appfundamentosReact/src/App.jsx
@@ -0,0 +1,29 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LandingPage from "./paginas/LandingPage";
+import Login from "./layout/Login";
+import FormularioLogin from "./paginas/FormularioLogin";
+import Dashboard from "./layout/Dashboard";
+import './App.css'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+                                
+        <Route path="/" element={<LandingPage />}>
+        </Route>
+
+        <Route path="/login" element={<Login />}>
+          <Route index element={<FormularioLogin />} />
+        </Route>
+
+        <Route path="/fundamentos" element={<Dashboard />}>
+        </Route>
+
+          
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
